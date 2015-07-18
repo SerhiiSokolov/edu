3a1cbb2d94d697fb0ea7b576d62b91141d80c981
dn130781ssv <dn0781@gmail.com>
2015-07-19 01:36:32 +0300

    0.0.5
    Add first try-catch-finally

--- hilllel/hw1/ifelse/ClassBase1.java
+++ hilllel/hw1/ifelse/ClassBase1.java
@@ -11,15 +11,29 @@ public class ClassBase1
 		int b=2;
 		int answer=testEven(a,b);
 		System.out.println(answer);
-		
+
 		//////////////////////////////////
 		//2.Определить какой четверти принадлежит точка с координатами (х,у)
 		/////////////////////////////////
-		int x=-2;
+		int x=0;
 		int y=2;
+		try
+		{
 		answer=testQuarter(x,y);
 		System.out.println(answer);
+		}
+		catch(IllegalArgumentException errObj)
 		
+		{
+			System.out.println("Error - "+errObj+". Zero not alowed");
+		}
+		finally 
+		{
+			x=2;
+			y=2;
+			answer=testQuarter(x,y);
+			System.out.println("Test \"finally\" after error with zero." +answer);
+		}
 		//////////////////////////////////
 		//3.Найти сумму только положительных из трех чисел
 		//////////////////////////////////
@@ -28,7 +42,7 @@ public class ClassBase1
 		int z=10;
 		answer=sumPositive(x,y,z);
 		System.out.println(answer);
-		
+
 		/////////////////////////////////
 		//4.Посчитать выражение макс(а*б*с, а+б+с)+3
 		/////////////////////////////////
@@ -37,7 +51,7 @@ public class ClassBase1
 		int c=3;
 		answer=findMax(a,b,c);
 		System.out.println(answer);
-		
+
 		/////////////////////////////////
 		//5.Написать программу определения оценки студента по его рейтингу
 		/////////////////////////////////
@@ -45,32 +59,31 @@ public class ClassBase1
 		char grade=findGrade(rating);
 		System.out.println(grade);
 	}
-	
+
 	public static int testEven(int a, int b)
 	{
 		if (a%2==0) return a*b;
 		else return a+b;
 	}
-	
+
 	public static int testQuarter(int x, int y)
 	{
 		int result=0;
-		if(x!=0 && y!=0)
+		if(x==0 || y==0)
+			throw new IllegalArgumentException();
+		if (x>=0) 
 		{
-			if (x>=0) 
-			{
-				if (y>=0) result=1;
-				else result=2;
-			}
-			else
-			{
-				if (y>0) result=4;
-				else result=3;
-			}
+			if (y>=0) result=1;
+			else result=2;
+		}
+		else
+		{
+			if (y>0) result=4;
+			else result=3;
 		}
 		return result;
 	}
-	
+
 	public static int sumPositive(int x, int y, int z)
 	{
 		int sum=0;
@@ -79,7 +92,7 @@ public class ClassBase1
 		if (z>=0) sum+=z;
 		return sum;
 	}
-	
+
 	public static int findMax(int x, int y, int z)
 	{
 		int result=0;
@@ -87,7 +100,7 @@ public class ClassBase1
 		else result=(x+y+z+3);
 		return result;
 	}
-	
+
 	public static char findGrade(int x)
 	{
 		char grade='F';
@@ -99,6 +112,6 @@ public class ClassBase1
 		else grade='A';
 		return grade;
 	}
-	
-	
+
+
 }
