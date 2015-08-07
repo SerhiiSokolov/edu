37bc094150ef230830d085f7bb0a825447fb74dd
dn130781ssv <dn0781@gmail.com>
2015-08-07 22:45:43 +0300

    Add Selenium test for Opera

--- hillel/hw6/testjs/HHCalcTes.java
+++ hillel/hw6/testjs/HHCalcTes.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HHCalcTes 
@@ -22,6 +24,7 @@ public class HHCalcTes
 		driver.findElement(By.id("btncount")).click();
 		String res = driver.findElement(By.id("txt1")).getAttribute("value");
 		assertEquals("108", res);
+		driver.quit();
 	}
 	@Test
 	public void testMinusMozilla() 
@@ -36,6 +39,7 @@ public class HHCalcTes
 		driver.findElement(By.id("btncount")).click();
 		String res = driver.findElement(By.id("txt1")).getAttribute("value");
 		assertEquals("-84", res);
+		driver.quit();
 	}
 	@Test
 	public void testUmnogMozilla() 
@@ -49,6 +53,7 @@ public class HHCalcTes
 		driver.findElement(By.id("btncount")).click();
 		String res = driver.findElement(By.id("txt1")).getAttribute("value");
 		assertEquals("60", res);
+		driver.quit();
 	}
 	@Test
 	public void testDevideMozilla() 
@@ -63,6 +68,7 @@ public class HHCalcTes
 		driver.findElement(By.id("btncount")).click();
 		String res = driver.findElement(By.id("txt1")).getAttribute("value");
 		assertEquals("3", res);
+		driver.quit();
 	}
 	
 	@Test
@@ -79,6 +85,7 @@ public class HHCalcTes
 		driver.findElement(By.id("btncount")).click();
 		String res = driver.findElement(By.id("txt1")).getAttribute("value");
 		assertEquals("108", res);
+		driver.quit();
 	}
 	@Test
 	public void testMinusChrome() 
@@ -94,6 +101,7 @@ public class HHCalcTes
 		driver.findElement(By.id("btncount")).click();
 		String res = driver.findElement(By.id("txt1")).getAttribute("value");
 		assertEquals("-84", res);
+		driver.quit();
 	}
 	@Test
 	public void testUmnogChrome() 
@@ -108,6 +116,7 @@ public class HHCalcTes
 		driver.findElement(By.id("btncount")).click();
 		String res = driver.findElement(By.id("txt1")).getAttribute("value");
 		assertEquals("60", res);
+		driver.quit();
 	}
 	@Test
 	public void testDevideChrome() 
@@ -123,5 +132,71 @@ public class HHCalcTes
 		driver.findElement(By.id("btncount")).click();
 		String res = driver.findElement(By.id("txt1")).getAttribute("value");
 		assertEquals("3", res);
+		driver.quit();
+	}
+
+	@Test
+	public void testPlusOpera() 
+	{
+		System.setProperty("webdriver.opera.driver", "d:/operadriver.exe");
+		WebDriver driver = new OperaDriver();
+		driver.get("file:///d:/Java/ex_calk.html");	
+		driver.findElement(By.id("btn1")).click();
+		driver.findElement(By.id("btn2")).click();
+		driver.findElement(By.id("btnplus")).click();
+		driver.findElement(By.id("btn9")).click();
+		driver.findElement(By.id("btn6")).click();
+		driver.findElement(By.id("btncount")).click();
+		String res = driver.findElement(By.id("txt1")).getAttribute("value");
+		assertEquals("108", res);
+		driver.quit();
+
+	}
+	@Test
+	public void testMinusOpera() 
+	{
+		System.setProperty("webdriver.opera.driver", "d:/operadriver.exe");
+		WebDriver driver = new OperaDriver();
+		driver.get("file:///d:/Java/ex_calk.html");	
+		driver.findElement(By.id("btn1")).click();
+		driver.findElement(By.id("btn2")).click();
+		driver.findElement(By.id("btnminus")).click();
+		driver.findElement(By.id("btn9")).click();
+		driver.findElement(By.id("btn6")).click();
+		driver.findElement(By.id("btncount")).click();
+		String res = driver.findElement(By.id("txt1")).getAttribute("value");
+		assertEquals("-84", res);
+		driver.quit();
+	}
+	@Test
+	public void testUmnogOpera() 
+	{
+		System.setProperty("webdriver.opera.driver", "d:/operadriver.exe");
+		WebDriver driver = new OperaDriver();
+		driver.get("file:///d:/Java/ex_calk.html");	
+		driver.findElement(By.id("btn1")).click();
+		driver.findElement(By.id("btn2")).click();
+		driver.findElement(By.id("btnumnog")).click();
+		driver.findElement(By.id("btn5")).click();
+		driver.findElement(By.id("btncount")).click();
+		String res = driver.findElement(By.id("txt1")).getAttribute("value");
+		assertEquals("60", res);
+		driver.quit();
+	}
+	@Test
+	public void testDevideOpera() 
+	{
+		System.setProperty("webdriver.opera.driver", "d:/operadriver.exe");
+		WebDriver driver = new OperaDriver();
+		driver.get("file:///d:/Java/ex_calk.html");	
+		driver.findElement(By.id("btn9")).click();
+		driver.findElement(By.id("btn9")).click();
+		driver.findElement(By.id("btndevide")).click();
+		driver.findElement(By.id("btn3")).click();
+		driver.findElement(By.id("btn3")).click();
+		driver.findElement(By.id("btncount")).click();
+		String res = driver.findElement(By.id("txt1")).getAttribute("value");
+		assertEquals("3", res);
+		driver.quit();
 	}
 }
