b626160b0972c89f0403d0f455723f6cec35fdbd
dn130781ssv <dn0781@gmail.com>
2015-08-13 09:43:13 +0300

    All querry ready

--- hillel/hw8/db/DBxx.java
+++ hillel/hw8/db/DBxx.java
@@ -12,24 +12,34 @@ public class DBxx {
 		Class.forName("org.h2.Driver");
 		Connection conn = DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
 		Statement st = conn.createStatement();
-		ResultSet rs = st.executeQuery("SELECT * FROM PERSON");
-		while(rs.next())
-		{
-			System.out.println(rs.getInt(1) + ", " + rs.getString(2) + ", " + rs.getString(3) + ", " + rs.getInt(4));
-		}
+		ResultSet rs;
+		
 		rs=st.executeQuery("SELECT FirstName, LastName, Age FROM PERSON WHERE AGE>=18 and AGE<=35");
 		while(rs.next())
 		{
 			System.out.println(rs.getString(1) + ", " + rs.getString(2) + ", " + rs.getInt(3));
 		}
-		rs=st.executeQuery("select LastName from Person where LastName like '%_í_%'");
+
+		rs=st.executeQuery("SELECT DISTINCT FirstName, LastName FROM PERSON WHERE LEFT(FirstName,1)=LEFT(LastName,1)");
+		while(rs.next())
+		{
+			System.out.println(rs.getString(1) + ", " + rs.getString(2));
+		}
+		
+		rs=st.executeQuery("SELECT Id, FirstName, LastName, Age FROM PERSON WHERE Id%2=0");
+		while(rs.next())
+		{
+			System.out.println(rs.getInt(1)+", " + rs.getString(2) + ", " + rs.getString(3));
+		}
+		
+		rs=st.executeQuery("select LastName from Person where LastName like '%_Ð½_%'");
 		while(rs.next())
 		{
 			System.out.println(rs.getString(1));
 		}
+		
 		rs.close();
 		st.close();
 		conn.close();
 	}
-	
 }
