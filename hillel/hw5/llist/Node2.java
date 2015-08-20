bc0d1afa513133e7422efbc5ed091fae0d825cba
dn130781ssv <dn0781@gmail.com>
2015-08-20 11:45:19 +0300

    Update Node2.java

--- hillel/hw5/llist/Node2.java
+++ hillel/hw5/llist/Node2.java
@@ -2,32 +2,11 @@ package edu.hillel.hw5.llist;
 
 public class Node2
 {
-	private int data;
-	private Node2 next;
-	private Node2 prev;
-	
-	public Node2 getPrev() {
-		return prev;
-	}
-	public void setPrev(Node2 prev) {
-		this.prev = prev;
-	}
-	
-	public int getData() 
-	{	
-		int temp=data;
-		return temp;
-	}
-	public void setData(int data) 
+	int data;
+	Node2 next;
+	Node2 prev;
+	Node2(int data)
 	{
-		this.data = data;
-	}
-	public Node2 getNext() 
-	{
-		return next;
-	}
-	public void setNext(Node2 next) 
-	{
-		this.next = next;
+		this.data=data;
 	}
 }
